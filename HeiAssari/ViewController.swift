6b7792571d20b39e062cb6004738728b748fa05e
seyoungTango <seyoung.arts.park@protonmail.com>
2016-10-17 18:20:57 +0300

    Initial Commit

---
+++ HeiAssari/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeiAssari
+//
+//  Created by Park Seyoung on 17/10/16.
+//  Copyright © 2016 Park Seyoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
