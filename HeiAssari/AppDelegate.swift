d0511d903f245bae2a5c2e2090765b1dd10cb4da
seyoungTango <seyoung.arts.park@protonmail.com>
2016-10-20 18:02:53 +0300

    Add demo gif

--- HeiAssari/AppDelegate.swift
+++ HeiAssari/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        UIApplication.shared.applicationIconBadgeNumber = 0
+        
         let rootVC = ViewController()
         let nav = UINavigationController()
         nav.viewControllers = [rootVC]
@@ -61,9 +63,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        UIApplication.shared.applicationIconBadgeNumber = 0
     }
-
 
 }
 
