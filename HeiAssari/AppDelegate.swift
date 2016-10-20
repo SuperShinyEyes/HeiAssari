99a22eb8799dfe2f8c68e904be67cac70e403562
seyoungTango <seyoung.arts.park@protonmail.com>
2016-10-20 16:26:34 +0300

    Show nav controller

--- HeiAssari/AppDelegate.swift
+++ HeiAssari/AppDelegate.swift
@@ -16,10 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
-        window?.makeKeyAndVisible()
+        let rootVC = ViewController()
+        let nav = UINavigationController()
+        nav.viewControllers = [rootVC]
         
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
         
         requestNotificationAuthorization()
         
